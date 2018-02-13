a0fbf1c0ce36f3d9f73256d96be15ec19cff05af
DESKTOP-D9GKM16\Dell <singhaps57@gmail.com>
2018-02-13 20:11:37 +0530

    Added Comment    vv

--- app/src/main/java/execute/devved/demoapp/activityClass/DetailsActivity.java
+++ app/src/main/java/execute/devved/demoapp/activityClass/DetailsActivity.java
@@ -25,7 +25,7 @@ import execute.devved.demoapp.utils.OnSwipeTouchListener;
 
 
 /**
- * Hi
+ * Hi  Arun
  */
 public class DetailsActivity extends AppCompatActivity {
     ArrayList<SmsEntity> lang_detail_list;
